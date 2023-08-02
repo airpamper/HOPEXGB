5211843ff3553eff38998ef47149b8a28bd3ff6d
airpamper <141115968+airpamper@users.noreply.github.com>
2023-08-02 15:37:27 +0800

    Update preprocess.py

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,76 +1,42 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
 import time
 import itertools
 
 #associations and interactions matrix
-def get_dis_mi_adjacency_matrix(dis_mi_link):
-    matrix = np.zeros((len(mi), len(dis)), dtype=np.float32)
-    for index in dis_mi_link.index:
-        matrix[dis_mi_link.loc[index, 'miRNA']][dis_mi_link.loc[index, 'disease']] = 1
-    return matrix
+class get_adjacency_matrix():
+    def __init__(self,mi,dis,lnc,dis_mi_link, lnc_mi_link, dis_lnc_link):
+
+        self.dis_mi_link = dis_mi_link
+        self.lnc_mi_link = lnc_mi_link
+        self.dis_lnc_link = dis_lnc_link
+        self.mi = mi
+        self.dis = dis
+        self.lnc = lnc
+        
+    def get_dis_mi_adjacency_matrix(self):
+        dis_mi_matrix = np.zeros((len(self.mi), len(self.dis)), dtype=np.float32)
+        for index in self.dis_mi_link.index:
+            dis_mi_matrix[self.dis_mi_link.loc[index, 'miRNA']][self.dis_mi_link.loc[index, 'disease']] = 1
+        return dis_mi_matrix
 
 
-def get_lnc_mi_adjacency_matrix(lnc_mi_link):
-    matrix = np.zeros((len(lnc), len(mi)), dtype=np.float32)
-    for index in lnc_mi_link.index:
-        matrix[lnc_mi_link.loc[index, 'lncRNA']][lnc_mi_link.loc[index, 'miRNA']] = 1
-    return matrix
+    def get_lnc_mi_adjacency_matrix(self):
+        lnc_mi_matrix = np.zeros((len(self.lnc), len(self.mi)), dtype=np.float32)
+        for index in self.lnc_mi_link.index:
+            lnc_mi_matrix[self.lnc_mi_link.loc[index, 'lncRNA']][self.lnc_mi_link.loc[index, 'miRNA']] = 1
+        return lnc_mi_matrix
 
 
-def get_dis_lnc_adjacency_matrix(dis_lnc_link):
-    matrix = np.zeros((len(lnc), len(dis)), dtype=np.float32)
-    for index in dis_lnc_link.index:
-        matrix[dis_lnc_link.loc[index, 'lncRNA']][dis_lnc_link.loc[index, 'disease']] = 1
-    return matrix
+    def get_dis_lnc_adjacency_matrix(self):
+        dis_lnc_matrix = np.zeros((len(self.lnc), len(self.dis)), dtype=np.float32)
+        for index in self.dis_lnc_link.index:
+            dis_lnc_matrix[dis_lnc_link.loc[index, 'lncRNA']][self.dis_lnc_link.loc[index, 'disease']] = 1
+        return dis_lnc_matrix
 
 
 #similarity
-
-def compute_GIP(matrix, gamma=1):
-    revised_gamma = matrix.shape[1] * gamma / np.sum(np.square(matrix))
-
-    GIP_matrix = np.zeros((matrix.shape[1], matrix.shape[1]), dtype=np.float32)
-    for idx_1 in range(matrix.shape[1]):
-        for idx_2 in range(matrix.shape[1]):
-            print(f'computing GIP {idx_2} / {matrix.shape[1]} for {idx_1} / {matrix.shape[1]}', end='\r')
-            GIP_matrix[idx_1][idx_2] = np.exp(- revised_gamma * np.sum(np.square(matrix[:, idx_1] - matrix[:, idx_2])))
-    return GIP_matrix
-
-
-def max_score(id, set):
-    global gene_id, gene_sim, m_gene_id
-    max_lls = 0.0
-    for g in set:
-        if g >= 0 and gene_sim[id][g] > max_lls:
-            max_lls = gene_sim[id][g]
-    return max_lls
-
-
-def compute_func_sim(i, j, t):
-    global genes, miRNA, miRNA1, m_gene_id
-    if i == j:
-        return 1.0
-    elif miRNA1[i] not in miRNA or miRNA1[j] not in miRNA:
-        return 0.0
-    else:
-        m1_gene = m_gene_id[i]
-        m2_gene = m_gene_id[j]
-        m1_gene_score = 0.0
-        m2_gene_score = 0.0
-        for k in range(len(m1_gene)):
-            if m1_gene[k] in m2_gene:
-                m1_gene_score += 1.0
-            elif m1_gene[k] >= 0:
-                m1_gene_score += max_score(m1_gene[k], m2_gene)
-        for k in range(len(m2_gene)):
-            if m2_gene[k] in m1_gene:
-                m2_gene_score += 1.0
-            elif m2_gene[k] >= 0:
-                m2_gene_score += max_score(m2_gene[k], m1_gene)
-        return (m1_gene_score + m2_gene_score) / (len(m1_gene) + len(m2_gene))
-
-    
 def semantic_value(theta, code, trs):
     code_split = code.split('.')
     code_len = len(code_split)
@@ -84,64 +50,6 @@ def semantic_value(theta, code, trs):
             trs[tmp_code] = code_value
         code_value = code_value * theta
         
-
-def miRNA_similarity(miRNA_gene_link,gene_gene_link_score,miss_miRNAlist):
-    gene_gene_link_score['score'] = (gene_gene_link_score['score'] - gene_gene_link_score['score'].min()) / (gene_gene_link_score['score'].max() - gene_gene_link_score['score'].min())
-
-    genes = set(gene_gene_link_score['EntrezGeneID1'].tolist()) | set(gene_gene_link_score['EntrezGeneID2'].tolist())
-    genes = list(genes)
-    gene_id = {g: i for i, g in enumerate(genes)}
-    
-    #mti means miRNA-targetgene-id  links from 
-    mti_genes = miRNA_gene_link['Target Gene (Entrez Gene ID)'].unique().tolist()
-    mti_sp_genes = list(set(mti_genes) - set(genes))
-    mti_sp_genes_id = {g: -i - 1 for i, g in enumerate(mti_sp_genes)}
-    
-    miRNA1 = miRNA_disease_link['miRNA'].unique().tolist()
-    miRNA2 = miRNA_gene_link['miRNA'].unique().tolist()
-    miRNA = list(set(miRNA1) & set(miRNA2))
-    miss_miRNA = list(set(miRNA1) - set(miRNA2))
-    miRNA_id = {m: i for i, m in enumerate(miRNA1)}
-    
-    gene_sim = np.zeros((len(genes), len(genes)), dtype=np.float64)
-    for idx in gene_gene_link_score.index:
-        gene1 = gene_gene_link_score.loc[idx, 'EntrezGeneID1']
-        gene2 = gene_gene_link_score.loc[idx, 'EntrezGeneID2']
-        gene_sim[gene_id[gene1]][gene_id[gene2]] = gene_gene_link_score.loc[idx, 'score']
-    
-    for idx in range(len(genes)):
-        gene_sim[idx][idx] = 1
-   
-    m_gene_id = []
-    for m in miRNA1:
-        m_gene = miRNA_gene_link.values[miRNA_gene_link.values[:, 0] == m][:, 1]
-        m_gene_id.append([])
-        for g in m_gene:
-            if g in genes:
-                m_gene_id[-1].append(gene_id[g])
-            else:
-                m_gene_id[-1].append(mti_sp_genes_id[g])
-    
-    func_sim = np.zeros((len(miRNA1), len(miRNA1)), dtype=np.float32)
-    t = time.time()
-    for i in range(len(miRNA1)):
-        for j in range(len(miRNA1)):
-            func_sim[i, j] = func_sim[j, i] = compute_func_sim(i, j, t)
-            
-    gip_sim = compute_GIP(dis_mi_matrix)
-    miss_list = miss_miRNA
-    mi_sim = np.zeros((len(miRNA1), len(miRNA1)), dtype=np.float32)
-    for idx_1 in range(len(miRNA1)):
-        for idx_2 in range(idx_1, len(miRNA1)):
-            if mi[idx_1] in miss_list or mi[idx_2] in miss_list:
-                mi_sim[idx_1, idx_2] = mi_sim[idx_2, idx_1] = gip_sim[idx_1, idx_2]
-            else:
-                mi_sim[idx_1, idx_2] = mi_sim[idx_2, idx_1] = func_sim[idx_1, idx_2]
-
-    mi_similarity = mi_sim
-    return mi_similarity
-
-     
 def disease_similarity(disease_name):
     disease_id = {d: i for i, d in enumerate(disease_name)}
     # mtrees is the mesh tree structure from MeSH database
@@ -189,9 +97,118 @@ def disease_similarity(disease_name):
 
 def lncRNA_similarity(lncRNA_expression):
     from sklearn.preprocessing import minmax_scale
+    #lncRNA_expression is from NONCODEV database
     lnc_spearman = lncRNA_expression.corr('spearman')
     lnc_similarity = minmax_scale(lnc_spearman, feature_range=(0, 1))
     return lnc_similarity
+    
+class miRNA_similarity():
+    
+    def __init__(self,dis_mi_matrix,gamma,miRNA_gene_link,gene_gene_link_score,miRNA_disease_link):
+
+        self.dis_mi_link = dis_mi_link        
+        self.gamma = gamma       
+        self.miRNA_gene_link = miRNA_gene_link
+        self.gene_gene_link_score = gene_gene_link_score
+        self.genes = list(set(gene_gene_link_score['EntrezGeneID1'].tolist()) | set(gene_gene_link_score['EntrezGeneID2'].tolist()))
+        self.miRNA_disease_link = miRNA_disease_link
+
+        
+    def compute_GIP(self):
+        revised_gamma = self.matrix.shape[1] * self.gamma / np.sum(np.square(self.matrix))    
+        GIP_matrix = np.zeros((self.matrix.shape[1], self.matrix.shape[1]), dtype=np.float32)
+        for idx_1 in range(self.matrix.shape[1]):
+            for idx_2 in range(self.matrix.shape[1]):
+                GIP_matrix[idx_1][idx_2] = np.exp(- revised_gamma * np.sum(np.square(self.matrix[:, idx_1] - self.matrix[:, idx_2])))
+        return GIP_matrix
+
+    def max_score(self,id, set):
+        global gene_id, gene_sim, m_gene_id
+        max_lls = 0.0
+        for g in set:
+            if g >= 0 and gene_sim[id][g] > max_lls:
+                max_lls = gene_sim[id][g]
+        return max_lls
+            
+    def compute_func_sim(self,i, j, t):
+        global miRNA, miRNA1, m_gene_id
+        
+        if i == j:
+            return 1.0
+        elif miRNA1[i] not in miRNA or miRNA1[j] not in miRNA:
+            return 0.0
+        else:
+            m1_gene = m_gene_id[i]
+            m2_gene = m_gene_id[j]
+            m1_gene_score = 0.0
+            m2_gene_score = 0.0
+            for k in range(len(m1_gene)):
+                if m1_gene[k] in m2_gene:
+                    m1_gene_score += 1.0
+                elif m1_gene[k] >= 0:
+                    m1_gene_score += miRNA_similarity.max_score(m1_gene[k], m2_gene)
+            for k in range(len(m2_gene)):
+                if m2_gene[k] in m1_gene:
+                    m2_gene_score += 1.0
+                elif m2_gene[k] >= 0:
+                    m2_gene_score += miRNA_similarity.max_score(m2_gene[k], m1_gene)
+            return (m1_gene_score + m2_gene_score) / (len(m1_gene) + len(m2_gene))       
+
+
+    def similarity(self):
+
+        gene_id = {g: i for i, g in enumerate(self.genes)}
+        
+        #mti means miRNA-targetgene-id  links from miRTarBase database
+        mti_genes = self.miRNA_gene_link['Target Gene (Entrez Gene ID)'].unique().tolist()
+        mti_sp_genes = list(set(mti_genes) - set(self.genes))
+        mti_sp_genes_id = {g: -i - 1 for i, g in enumerate(mti_sp_genes)}
+        
+        miRNA1 = self.miRNA_disease_link['miRNA'].unique().tolist()
+        miRNA2 = self.miRNA_gene_link['miRNA'].unique().tolist()
+        miRNA = list(set(miRNA1) & set(miRNA2))
+        miss_miRNA = list(set(miRNA1) - set(miRNA2))
+        #miRNA_id = {m: i for i, m in enumerate(miRNA1)}
+        
+        gene_sim = np.zeros((len(self.genes), len(self.genes)), dtype=np.float64)
+        for idx in self.gene_gene_link_score.index:
+            gene1 = self.gene_gene_link_score.loc[idx, 'EntrezGeneID1']
+            gene2 = self.gene_gene_link_score.loc[idx, 'EntrezGeneID2']
+            gene_sim[gene_id[gene1]][gene_id[gene2]] = self.gene_gene_link_score.loc[idx, 'score']
+        
+        for idx in range(len(self.genes)):
+            gene_sim[idx][idx] = 1
+       
+        m_gene_id = []
+        for m in miRNA1:
+            m_gene = self.miRNA_gene_link.values[self.miRNA_gene_link.values[:, 0] == m][:, 1]
+            m_gene_id.append([])
+            for g in m_gene:
+                if g in self.genes:
+                    m_gene_id[-1].append(gene_id[g])
+                else:
+                    m_gene_id[-1].append(mti_sp_genes_id[g])
+        
+        func_sim = np.zeros((len(miRNA1), len(miRNA1)), dtype=np.float32)
+        t = time.time()
+        for i in range(len(miRNA1)):
+            for j in range(len(miRNA1)):
+                func_sim[i, j] = func_sim[j, i] = miRNA_similarity.compute_func_sim(i, j, t)
+                
+        gip_sim = miRNA_similarity.compute_GIP()
+        miss_list = miss_miRNA
+        mi_sim = np.zeros((len(miRNA1), len(miRNA1)), dtype=np.float32)
+        for idx_1 in range(len(miRNA1)):
+            for idx_2 in range(idx_1, len(miRNA1)):
+                if mi[idx_1] in miss_list or mi[idx_2] in miss_list:
+                    mi_sim[idx_1, idx_2] = mi_sim[idx_2, idx_1] = gip_sim[idx_1, idx_2]
+                else:
+                    mi_sim[idx_1, idx_2] = mi_sim[idx_2, idx_1] = func_sim[idx_1, idx_2]
+    
+        mi_similarity = mi_sim
+        return mi_similarity
+
+
 
 
 if __name__ == "__main__":
@@ -203,9 +220,10 @@ if __name__ == "__main__":
     mi = dis_mi_link['mi'].unique().tolist()
     lnc = dis_lnc_link['lnc'].unique().tolist()
     
-    dis_mi_matrix = get_dis_mi_adjacency_matrix(dis_mi_link)
-    dis_lnc_matrix = get_dis_lnc_adjacency_matrix(dis_lnc_link)
-    lnc_mi_matrix = get_lnc_mi_adjacency_matrix(lnc_mi_link)
+    get_matrix = get_adjacency_matrix(mi,dis,lnc,dis_mi_link, lnc_mi_link, dis_lnc_link)
+    dis_mi_matrix = get_matrix.get_dis_mi_adjacency_matrix()
+    dis_lnc_matrix = get_matrix.get_dis_lnc_adjacency_matrix()
+    lnc_mi_matrix = get_matrix.get_lnc_mi_adjacency_matrix()
     
     unlabeled = []
     d = [i for i in range(dis)]
@@ -219,12 +237,13 @@ if __name__ == "__main__":
             unlabeled.append(item2)     
     unlabeled = np.array(unlabeled) 
     
-    mi_similarity = miRNA_similarity()
+    mi_similarity = miRNA_similarity.similarity()
     dis_similarity = disease_similarity()
     lnc_similarity = lncRNA_similarity()
 
     sim_m = np.concatenate([mi_similarity, dis_mi_matrix, lnc_mi_matrix.T], axis=1)
     sim_d = np.concatenate([dis_mi_matrix.T, dis_similarity,dis_lnc_matrix.T], axis=1)
     sim_l = np.concatenate([lnc_mi_matrix, dis_lnc_matrix, lnc_similarity], axis=1)
+    #The heterogeneous disease-miRNA-lncRNA network
     sim_all = np.concatenate([sim_m, sim_d, sim_l], axis=0)
     #np.save("all_feature_adj", sim_all)
